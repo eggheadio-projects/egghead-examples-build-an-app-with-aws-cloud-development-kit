84440420841d581343b4298a86ad52e8fa56594a
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-04-19 12:20:25 +0200

    Added lessons 5-7

---
+++ lessons/6-change-proprties-of-a-lambda-function-deployed-with-cdk/todo-app/lib/todo-app-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+
+export class TodoAppStack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const helloLambda = new lambda.Function(this, "HelloLambda", {
+            code: lambda.Code.fromAsset("lambda"),
+            handler: "hello.handler",
+            runtime: lambda.Runtime.NODEJS_12_X,
+            memorySize: 256,
+            timeout: cdk.Duration.seconds(10)
+        });
+    }
+}
