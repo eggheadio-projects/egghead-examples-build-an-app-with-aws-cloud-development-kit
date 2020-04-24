1d13320450a014879d745741706e72f5267aa263
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-04-24 13:44:03 +0200

    Added lessons 12 and 13

---
+++ lessons/12-trigger-lambda-function-on-s3-bucket-file-upload-with-cdk/todo-app/lib/todo-app-stack.ts
@@ -0,0 +1,33 @@
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apiGateway from "@aws-cdk/aws-apigateway";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3Notifications from "@aws-cdk/aws-s3-notifications";
+
+export class TodoAppStack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const helloLambda = new lambda.Function(this, "HelloLambda", {
+            code: lambda.Code.fromAsset("lambda"),
+            handler: "hello.handler",
+            runtime: lambda.Runtime.NODEJS_12_X,
+            memorySize: 256,
+            timeout: cdk.Duration.seconds(10),
+            environment: { isProduction: "absolutely not" }
+        });
+
+        new apiGateway.LambdaRestApi(this, "Endpoint", {
+            handler: helloLambda
+        });
+
+        const logoBucket = new s3.Bucket(this, "LogoBucket", {
+            publicReadAccess: true
+        });
+
+        logoBucket.addEventNotification(
+            s3.EventType.OBJECT_CREATED,
+            new s3Notifications.LambdaDestination(helloLambda)
+        );
+    }
+}
