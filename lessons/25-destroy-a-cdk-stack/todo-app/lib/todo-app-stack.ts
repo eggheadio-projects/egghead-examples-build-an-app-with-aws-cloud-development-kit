0df9f837f3e5a4f3f5aec1b892e38ff73c436e71
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-05-03 19:15:44 +0200

    Added last lessons

---
+++ lessons/25-destroy-a-cdk-stack/todo-app/lib/todo-app-stack.ts
@@ -0,0 +1,36 @@
+import * as cdk from "@aws-cdk/core";
+import * as apiGateway from "@aws-cdk/aws-apigateway";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
+import { TodoBackend } from "./todo-backend";
+import { SPADeploy } from "cdk-spa-deploy";
+
+export class TodoAppStack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const todoBackend = new TodoBackend(this, "TodoBackend");
+
+        new apiGateway.LambdaRestApi(this, "Endpoint", {
+            handler: todoBackend.handler
+        });
+
+        const logoBucket = new s3.Bucket(this, "LogoBucket", {
+            publicReadAccess: true
+        });
+
+        new s3Deployment.BucketDeployment(this, "DeployLogo", {
+            destinationBucket: logoBucket,
+            sources: [s3Deployment.Source.asset("./assets")]
+        });
+
+        new cdk.CfnOutput(this, "LogoPath", {
+            value: `https://${logoBucket.bucketDomainName}/egghead-logo.png`
+        });
+
+        new SPADeploy(this, "WebsiteDeploy").createSiteWithCloudfront({
+            indexDoc: "index.html",
+            websiteFolder: "../frontend/build"
+        });
+    }
+}
